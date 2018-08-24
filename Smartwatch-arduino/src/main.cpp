2c8915df2dc8c3d804c70d813f2bce302c76e250
Edwin Clement <edwinclement08@gmail.com>
2018-08-25 01:40:23 +0530

    initial draft

---
+++ Smartwatch-arduino/src/main.cpp
@@ -0,0 +1,109 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char *mqttServer = "192.168.0.103";
+const int mqttPort = 1884;
+const char *mqttUser = "";
+const char *mqttPassword = "";
+
+const char *ssid = "TP-LINK_E352";
+const char *password = "temp12345";
+
+int count = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+
+  Serial.println();
+  Serial.println("-----------------------");
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+
+  while (!client.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword))
+    {
+
+      Serial.println("connected");
+    }
+    else
+    {
+
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+
+  client.subscribe("esp/test");
+
+
+}
+
+void loop()
+{
+  count += 1;
+  client.loop();
+
+  char buf[100];
+  String s = "count : =  ";
+  s.toCharArray(buf, s.length());
+  String m1 = String(count);
+
+  // for(int h = 0; h < m1.length(); h++)  {
+  //   buf[h+  s.length() ]
+  // }
+  m1.toCharArray(buf +  s.length() - 1 , 100 - s.length()+1);
+
+
+
+  client.publish("esp/test", buf );
+  delay(3000);
+  while (!client.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword))
+    {
+
+      Serial.println("connected");
+      client.subscribe("esp/test");
+    }
+    else
+    {
+
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
---
+++ smartwatch-nodejs/index.js
@@ -0,0 +1,95 @@
+var mosca = require('mosca');
+
+
+var ascoltatore = {
+    //using ascoltatore
+    type: 'mqtt',
+    json: false,
+    mqtt: require('mqtt'),
+    host: '192.168.0.103',
+    port: 1883
+};
+
+var settings = {
+    port: 1884,
+    backend: ascoltatore
+};
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function (client) {
+    console.log('client connected', client.id);
+});
+
+// // fired when a message is received
+// server.on('published', function (packet, client) {
+//     console.log('Published', packet.payload);
+//     console.log('Published', packet);
+//     console.log('Published', packet.topic);
+//     console.log('Published', packet.payload.toString());
+
+
+// });
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+    console.log('Mosca server is up and running');
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+var firebase = require("firebase");
+
+var config = {
+    apiKey: "AIzaSyB-vlQ-2GFWo0Uo4pAbkjyR-noA_9i3m3g",
+    authDomain: "smartwatch-210415.firebaseapp.com",
+    databaseURL: "https://smartwatch-210415.firebaseio.com",
+    projectId: "smartwatch-210415",
+    storageBucket: "smartwatch-210415.appspot.com",
+    messagingSenderId: "980837883381"
+};
+
+
+
+var app = firebase.initializeApp(config);
+var database = firebase.database();
+
+var main_ref_location = 'watch/';
+
+function addTelemetry(watchId, heartRate,fall){
+    var ref = database.ref('watch/' + watchId)
+
+    return ref.set({
+        heartRate: heartRate,
+        fall: fall,
+    }, function(error) {
+        if (error) {
+          // The write failed...
+          console.log("error := " + watchId);      
+        } else {
+          // Data saved successfully!
+        //   console.log("done");
+          ref.off();
+          return;
+        }
+      });
+}
+
+addTelemetry("1", 234,0)
+
+console.log("Test")
+// app.delete()
+
+
+
+// fired when a message is received
+server.on('published', function (packet, client) {
+
+    // console.log('Published', packet.topic);
+    console.log('Published', packet.payload.toString());
+    var text =  packet.payload.toString();
+    addTelemetry(packet.client,text, 0 );
+});
