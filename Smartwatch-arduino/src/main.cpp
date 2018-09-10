b071be87048fa203589b1c07d73fbed2c31c9fd6
Edwin Clement <edwinclement08@gmail.com>
2018-09-10 18:21:20 +0530

    Updating

--- Smartwatch-arduino/src/main.cpp
+++ Smartwatch-arduino/src/main.cpp
@@ -1,19 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Need Mosquitto server on 1883 <- as a ubuntu-snap. restart system after install
-// need my server on 1884
 
 
-const char *mqttServer = "192.168.43.175";
+
+const char *mqttServer = "192.168.0.121";
 const int mqttPort = 1885;
 const char *mqttUser = "";
 const char *mqttPassword = "";
 
-const char *ssid = "ECUA";
-const char *password = "godofwar";
+const char *ssid = "TP-Link_6329";
+const char *password = "2051313908";
 
 int count = 0;
+long timeRecord;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -36,8 +36,8 @@ void callback(char *topic, byte *payload, unsigned int length)
 
 void setup()
 {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
+  timeRecord = millis();
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED)
@@ -56,12 +56,10 @@ void setup()
 
     if (client.connect("ESP8266Client", mqttUser, mqttPassword))
     {
-
       Serial.println("connected");
     }
     else
     {
-
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
@@ -74,37 +72,35 @@ void setup()
 }
 
 void loop()
-{
-  count += 1;
-  client.loop();
+{    client.loop();
 
-  char buf[100];
-  String s = "count : =  ";
-  s.toCharArray(buf, s.length());
-  String m1 = String(count);
+  if(millis() - timeRecord > 1000) {
 
-  // for(int h = 0; h < m1.length(); h++)  {
-  //   buf[h+  s.length() ]
-  // }
-  m1.toCharArray(buf +  s.length() - 1 , 100 - s.length()+1);
+    count += 1;
 
+    char buf[100];
+    String s = "count : =  ";
+    s.toCharArray(buf, s.length());
+    String m1 = String(count);
 
+    m1.toCharArray(buf +  s.length() - 1 , 100 - s.length()+1);
 
-  client.publish("esp/test", buf );
-  delay(3000);
+    client.publish("esp/test", buf );
+    timeRecord = millis();
+  }
+
+  // delay(3000);
   while (!client.connected())
   {
     Serial.println("Connecting to MQTT...");
 
     if (client.connect("ESP8266Client", mqttUser, mqttPassword))
     {
-
       Serial.println("connected");
       client.subscribe("esp/test");
     }
     else
     {
-
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
